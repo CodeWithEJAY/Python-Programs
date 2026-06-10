a35087fe9876ece94f0e2579349ad668b8729d49
Erickson Joseph Kristoffer Luna <ejkristofferluna@gmail.com>
2026-06-10 09:35:46 +0800

    AreaOfACircle.py

---
+++ AreaOfACircle.py
@@ -0,0 +1,8 @@
+# Exercise 4: Area of a Circle
+
+import math
+
+radius = float(input("Enter the radius of a circle: "))
+area = math.pi * pow(radius, 2)
+
+print(f"The area of the circle is {round(area, 2)} cm²")
